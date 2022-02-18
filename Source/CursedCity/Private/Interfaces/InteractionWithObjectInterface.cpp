a1db25c30e871398d4c83d54f2832e70a02bd2b3
Stich <SkyandSmurf@gmail.com>
2022-02-18 03:16:47 +0300

    Create interacation with object interface

---
+++ Source/CursedCity/Private/Interfaces/InteractionWithObjectInterface.cpp
@@ -0,0 +1,7 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/InteractionWithObjectInterface.h"
+#include "Player/Character/CursedCityCharacter.h"
+
+// Add default functionality here for any IInteractionWithObjectInterface functions that are not pure virtual.
